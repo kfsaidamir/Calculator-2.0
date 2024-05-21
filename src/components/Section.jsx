e5e7ca6373e444393a15308965c2b3b45c07f5fb
kf_saidamir <skadirov2009@gmail.com>
2024-05-21 16:53:53 +0500

    a

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,213 +1,214 @@
-    import React, { useRef, useState } from "react";
-    import { Box, Text, Flex, Container, Button, Input } from "@chakra-ui/react";
-    import { MdOutlineSubdirectoryArrowLeft } from "react-icons/md";
-    import { FaDeleteLeft } from "react-icons/fa6";
-    import axios from 'axios';
-    import {
-        Drawer,
-        DrawerBody,
-        DrawerFooter,
-        DrawerHeader,
-        DrawerOverlay,
-        DrawerContent,
-        DrawerCloseButton,
-        useDisclosure
-    } from '@chakra-ui/react';
+import React, { useRef, useState } from "react";
+import { Box, Text, Flex, Container, Button, Input } from "@chakra-ui/react";
+import { MdOutlineSubdirectoryArrowLeft } from "react-icons/md";
+import { FaDeleteLeft } from "react-icons/fa6";
+import axios from 'axios';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure
+} from '@chakra-ui/react';
 
-    const Section = () => {
-        const { isOpen, onOpen, onClose } = useDisclosure();
-        const btnRef = useRef();
+const Section = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = useRef();
 
-        const [counter, setCounter] = useState("0");
-        const [result, setResult] = useState(0);
-        const [operator, setOperator] = useState("");
-        const [name, setName] = useState("");
-        const [chatId, setChatId] = useState("-4287839022");
+    const [counter, setCounter] = useState("0");
+    const [result, setResult] = useState(0);
+    const [operator, setOperator] = useState("");
+    const [name, setName] = useState("");
+    const [chatId, setChatId] = useState("-4287839022");
 
-        // Пример массива товаров для демонстрации
-        const items = [
-            { name: 'Продукт 1', price: 10 },
-            { name: 'Продукт 2', price: 20 },
-        ];
+    // Пример массива товаров для демонстрации
+    const items = [
+        { name: 'Продукт 1', price: 10 },
+        { name: 'Продукт 2', price: 20 },
+    ];
 
-        const handleButtonClick = (value) => {
-            if (counter.length < 12) {
-                let newCounter = counter.replace(/\t/g, "");
-                newCounter = newCounter === "0" ? value.toString() : newCounter + value.toString();
-                for (let i = newCounter.length - 3; i > 0; i -= 3) {
-                    newCounter = newCounter.slice(0, i) + "\t" + newCounter.slice(i);
-                }
-                setCounter(newCounter);
+    const handleButtonClick = (value) => {
+        if (counter.length < 12) {
+            let newCounter = counter.replace(/\t/g, "");
+            newCounter = newCounter === "0" ? value.toString() : newCounter + value.toString();
+            for (let i = newCounter.length - 3; i > 0; i -= 3) {
+                newCounter = newCounter.slice(0, i) + "\t" + newCounter.slice(i);
             }
-        };
+            setCounter(newCounter);
+        }
+    };
 
-        const handleOperatorClick = (op) => {
-            if (operator === "") {
-                setResult(parseFloat(counter.replace(/\t/g, "")));
-                setOperator(op);
-                setCounter("0");
-            } else {
-                handleEqualClick();
-                setOperator(op);
-            }
-        };
-
-        const handleEqualClick = () => {
-            const currentValue = parseFloat(counter.replace(/\t/g, ""));
-            let res = 0;
-            switch (operator) {
-                case "+":
-                    res = result + currentValue;
-                    break;
-                case "-":
-                    res = result - currentValue;
-                    break;
-                case "*":
-                    res = result * currentValue;
-                    break;
-                case "/":
-                    res = result / currentValue;
-                    break;
-                default:
-                    res = currentValue;
-            }
-            setResult(res);
-            setCounter(res.toString());
-            setOperator("");
-        };
-
-        const Del = () => {
-            if (counter.length > 1) {
-                setCounter(counter.slice(0, -1));
-            } else {
-                setCounter("0");
-            }
-        };
-
-        const Delt = () => {
+    const handleOperatorClick = (op) => {
+        if (operator === "") {
+            setResult(parseFloat(counter.replace(/\t/g, "")));
+            setOperator(op);
             setCounter("0");
-            setResult(0);
-            setOperator("");
-        };
+        } else {
+            handleEqualClick();
+            setOperator(op);
+        }
+    };
 
-        const TelegramPost = () => {
-            if (chatId.trim() === "") {
-                alert("Пожалуйста, введите корректный идентификатор чата.");
-                return;
-            }
+    const handleEqualClick = () => {
+        const currentValue = parseFloat(counter.replace(/\t/g, ""));
+        let res = 0;
+        switch (operator) {
+            case "+":
+                res = result + currentValue;
+                break;
+            case "-":
+                res = result - currentValue;
+                break;
+            case "*":
+                res = result * currentValue;
+                break;
+            case "/":
+                res = result / currentValue;
+                break;
+            default:
+                res = currentValue;
+        }
+        setResult(res);
+        setCounter(res.toString());
+        setOperator("");
+    };
 
-            axios.post(`https://api.telegram.org/bot7156539137:AAGTyPWVBv2ENtrM7ZFowKWQDYqGNKnwnws/sendMessage`, {
-                chat_id: chatId,
-                text: `
+    const Del = () => {
+        if (counter.length > 1) {
+            setCounter(counter.slice(0, -1));
+        } else {
+            setCounter("0");
+        }
+    };
+
+    const Delt = () => {
+        setCounter("0");
+        setResult(0);
+        setOperator("");
+    };
+
+    const TelegramPost = () => {
+        if (chatId.trim() === "") {
+            alert("Пожалуйста, введите корректный идентификатор чата.");
+            return;
+        }
+
+        axios.post(`https://api.telegram.org/bot7156539137:AAGTyPWVBv2ENtrM7ZFowKWQDYqGNKnwnws/sendMessage`, {
+            chat_id: chatId,
+            text: `
             
             <b>Comment: ${name}</b>
             
                 `,
-                parse_mode: 'html'
-            })
-            .then(response => {
-                console.log("Сообщение отправлено:", response.data);
-            })
-            .catch(error => {
-                console.error("Ошибка отправки сообщения:", error);
-            });
+            parse_mode: 'html'
+        })
+        .then(response => {
+            console.log("Сообщение отправлено:", response.data);
+            setName(""); // Clear the input field after sending the message
+        })
+        .catch(error => {
+            console.error("Ошибка отправки сообщения:", error);
+        });
 
-            
-        };
-
-        return (
-            <>
-                <Box w={"full"} h={"100vh"}>
-
-                    <Button ref={btnRef} marginTop={"2%"} marginLeft={{ md: "89%", base: "30%" }} onClick={onOpen}>
-                        Оцените нас
-                    </Button>
-                    <Drawer
-                        isOpen={isOpen}
-                        placement='right'
-                        onClose={onClose}
-                        finalFocusRef={btnRef}
-                    >
-                        <DrawerOverlay />
-                        <DrawerContent>
-                            <DrawerCloseButton />
-                            <DrawerHeader>Напишите своё мнение </DrawerHeader>
-
-                            <DrawerBody>
-                                <Input placeholder='Пишите...' value={name} onChange={(e) => setName(e.target.value)} />
-                                {/* Идентификатор чата установлен по умолчанию */}
-                            </DrawerBody>
-
-                            <DrawerFooter>
-                                <Button variant='outline' mr={3} onClick={onClose}>
-                                    Отмена
-                                </Button>
-                                <Button colorScheme='blue' onClick={TelegramPost}>Отправить</Button>
-                            </DrawerFooter>
-                        </DrawerContent>
-                    </Drawer>
-
-                    <Container maxW={"container.xl"}>
-                        <Box
-                            maxW={{ md: "30%", base: "90%" }}
-                            bgColor={"white"}
-                            h={"50vh"}
-                            marginTop={"5%"}
-                            border={"1px solid gray"}
-                            padding={"10px"}
-                            borderRadius={"10px"}
-                        >
-                            <Container>
-                                <Flex alignItems={"center"} justifyContent={"space-between"}>
-                                    <Text>{counter}</Text>
-                                    <Flex gap={"10px"}>
-                                        <Button onClick={Del}>
-                                            <MdOutlineSubdirectoryArrowLeft />
-                                        </Button>
-                                        <Button onClick={Delt} marginRight={"11px"}>
-                                            <FaDeleteLeft />
-                                        </Button>
-                                    </Flex>
-                                </Flex>
-                                <Box
-                                    w={"100%"}
-                                    h={"1px"}
-                                    bgColor={"gray"}
-                                    marginTop={"15px"}
-                                ></Box>
-                                <Box>
-                                    <Flex gap={"10px"} marginTop={"5%"}>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(1)}>1</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(2)}>2</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(3)}>3</Button>
-                                    </Flex>
-                                    <Flex gap={"10px"} marginTop={"5%"}>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(4)}>4</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(5)}>5</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(6)}>6</Button>
-                                    </Flex>
-                                    <Flex gap={"10px"} marginTop={"5%"}>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(7)}>7</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(8)}>8</Button>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(9)}>9</Button>
-                                    </Flex>
-                                    <Flex gap={"10px"} marginTop={"5%"}>
-                                        <Button w={"30%"} onClick={() => handleButtonClick(0)}>0</Button>
-                                        <Button w={"30%"} onClick={() => handleOperatorClick("+")}>+</Button>
-                                        <Button w={"30%"} onClick={() => handleOperatorClick("-")}>-</Button>
-                                    </Flex>
-                                    <Flex gap={"10px"} marginTop={"5%"}>
-                                        <Button w={"30%"} onClick={() => handleOperatorClick("*")}>*</Button>
-                                        <Button w={"30%"} onClick={() => handleOperatorClick("/")}>/</Button>
-                                        <Button w={"30%"} onClick={handleEqualClick}>=</Button>
-                                    </Flex>
-                                </Box>
-                            </Container>
-                        </Box>
-                    </Container>
-                </Box>
-            </>
-        );
+        
     };
 
-    export default Section;
+    return (
+        <>
+            <Box w={"full"} h={"100vh"}>
+
+                <Button ref={btnRef} marginTop={"2%"} marginLeft={{ md: "89%", base: "30%" }} onClick={onOpen}>
+                    Оцените нас
+                </Button>
+                <Drawer
+                    isOpen={isOpen}
+                    placement='right'
+                    onClose={onClose}
+                    finalFocusRef={btnRef}
+                >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader>Напишите своё мнение </DrawerHeader>
+
+                        <DrawerBody>
+                            <Input placeholder='Пишите...' value={name} onChange={(e) => setName(e.target.value)} />
+                            {/* Идентификатор чата установлен по умолчанию */}
+                        </DrawerBody>
+
+                        <DrawerFooter>
+                            <Button variant='outline' mr={3} onClick={onClose}>
+                                Отмена
+                            </Button>
+                            <Button colorScheme='blue' onClick={TelegramPost}>Отправить</Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </Drawer>
+
+                <Container maxW={"container.xl"}>
+                    <Box
+                        maxW={{ md: "30%", base: "90%" }}
+                        bgColor={"white"}
+                        h={"50vh"}
+                        marginTop={"5%"}
+                        border={"1px solid gray"}
+                        padding={"10px"}
+                        borderRadius={"10px"}
+                    >
+                        <Container>
+                            <Flex alignItems={"center"} justifyContent={"space-between"}>
+                                <Text>{counter}</Text>
+                                <Flex gap={"10px"}>
+                                    <Button onClick={Del}>
+                                        <MdOutlineSubdirectoryArrowLeft />
+                                    </Button>
+                                    <Button onClick={Delt} marginRight={"11px"}>
+                                        <FaDeleteLeft />
+                                    </Button>
+                                </Flex>
+                            </Flex>
+                            <Box
+                                w={"100%"}
+                                h={"1px"}
+                                bgColor={"gray"}
+                                marginTop={"15px"}
+                            ></Box>
+                            <Box>
+                                <Flex gap={"10px"} marginTop={"5%"}>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(1)}>1</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(2)}>2</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(3)}>3</Button>
+                                </Flex>
+                                <Flex gap={"10px"} marginTop={"5%"}>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(4)}>4</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(5)}>5</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(6)}>6</Button>
+                                </Flex>
+                                <Flex gap={"10px"} marginTop={"5%"}>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(7)}>7</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(8)}>8</Button>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(9)}>9</Button>
+                                </Flex>
+                                <Flex gap={"10px"} marginTop={"5%"}>
+                                    <Button w={"30%"} onClick={() => handleButtonClick(0)}>0</Button>
+                                    <Button w={"30%"} onClick={() => handleOperatorClick("+")}>+</Button>
+                                    <Button w={"30%"} onClick={() => handleOperatorClick("-")}>-</Button>
+                                </Flex>
+                                <Flex gap={"10px"} marginTop={"5%"}>
+                                    <Button w={"30%"} onClick={() => handleOperatorClick("*")}>*</Button>
+                                    <Button w={"30%"} onClick={() => handleOperatorClick("/")}>/</Button>
+                                    <Button w={"30%"} onClick={handleEqualClick}>=</Button>
+                                </Flex>
+                            </Box>
+                        </Container>
+                    </Box>
+                </Container>
+            </Box>
+        </>
+    );
+};
+
+export default Section;
